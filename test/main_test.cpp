b10ee174965008230602214a8e29385270042810
AndLvovSky <viktorilvovskyi@ukr.net>
2019-04-08 12:40:33 +0300

    Added unit test for Fleet

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,6 +1,7 @@
 #include <QtTest/QtTest>
 #include "test_square.h"
 #include "test_ship.h"
+#include "test_fleet.h"
 
 int main(int argc, char* argv[]) {
     int status = 0;
@@ -10,6 +11,10 @@ int main(int argc, char* argv[]) {
     }
     {
         TestShip obj;
+        status |= QTest::qExec(&obj, argc, argv);
+    }
+    {
+        TestFleet obj;
         status |= QTest::qExec(&obj, argc, argv);
     }
     return status;
